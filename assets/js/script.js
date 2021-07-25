89a7af7fcb1ff1570dad947af3c42baf508ea9dc
MarcosViny <viniciusm896@gmail.com>
2021-07-25 17:43:04 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+/* ScrollReveal.js */
+const sr = ScrollReveal({
+origin: 'top',
+distance: '50px',
+duration: 2000,
+reset: false
+});
+
+ScrollReveal().reveal('.header', { delay: 100 });
+ScrollReveal().reveal('.card', { delay: 200 });
+ScrollReveal().reveal('.contact', { delay: 300 });
